437388ac47523fc5de998bd72da87ea094eaa38e
TriTran <thanhtritran8@gmail.com>
2023-05-29 09:48:25 +0700

    upload source code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
+const API_URL = "https://api.github.com"
+
+async function fetchResults(query) {
+  try {
+    const respone = await fetch(`${API_URL}/search/users?q=${query}`)
+    const json = await respone.json()
+    return json.items || []
+  } catch (er) {
+    throw new Error(er)
+  }
+}
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [results, setResults] = useState([])
+
+  function changeInput(e) {
+    setQuery(e.target.value)
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const results = await fetchResults(query)
+    setResults(results)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className='main'>
+        <h1 className='project-name'>Project 5: GitHub User Search</h1>
+        <Form
+          changeInput={changeInput}
+          handleSubmit={handleSubmit}
+          value={query}
+        />
+        <h3 className='results'>Results</h3>
+        {results.map((user) => (
+          <Users
+            key={user.id}
+            avatar={user.avatar_url}
+            url={user.html_url}
+            username={user.login}
+          />
+        ))}
+      </main>
     </div>
   );
+}
+
+function Form({changeInput, handleSubmit, value}) {
+  return (
+    <form onSubmit={handleSubmit} className='form-search'>
+      <input type='text' placeholder='Enter your username...' onChange={changeInput} className='form-input'/>
+      <button className='form-btn'>Search</button>
+    </form>
+  )
+}
+
+function Users({ avatar, url, username }) {
+  return (
+    <div className="user">
+      <img src={avatar} alt="Profile" width="50" height="50" />
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        {username}
+      </a>
+    </div>
+  )
 }
 
 export default App;
